b5d46e8005b58e853c6c073efe2f32aa0601ea48
vladislavkudryakov <zippo585@gmail.com>
2017-11-26 23:24:14 +0500

    Initial commit

---
+++ app/src/main/java/com/example/vladislavkudryakov/vk_3/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.vladislavkudryakov.vk_3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/*
+import android.util.Log;
+import android.widget.Toast;
+
+import com.arellomobile.mvp.MvpAppCompatActivity;
+import com.arellomobile.mvp.presenter.InjectPresenter;
+import com.example.user.fandroidvktest.consts.ApiConstants;
+import com.example.user.fandroidvktest.mvp.presenter.MainPresenter;
+import com.example.user.fandroidvktest.mvp.view.MainView;
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.util.VKUtil;
+*/
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+/*
+        @Override
+        protected void attachBaseContext(Context base) {
+            super.attachBaseContext(base);
+            MultiDex.install(this);
+        }*/
+    }
+}
