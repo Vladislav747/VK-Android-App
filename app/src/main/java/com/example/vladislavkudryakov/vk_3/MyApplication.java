3e17dd4c3b51c7f7e5e061b44f02aa52bf7d31a5
Vladislav747 <zippo585@gmail.com>
2018-02-20 02:46:59 +0500

    Continue to code - module 1
    Need comments

--- app/src/main/java/com/example/vladislavkudryakov/vk_3/MyApplication.java
+++ app/src/main/java/com/example/vladislavkudryakov/vk_3/MyApplication.java
@@ -2,9 +2,7 @@ package com.example.vladislavkudryakov.vk_3;
 
 import android.app.Application;
 import com.vk.sdk.VKSdk;
-/**
- * Created by Melo on 19.02.2018.
- */
+
 
 public class MyApplication extends Application {
 
@@ -12,7 +10,7 @@ public class MyApplication extends Application {
     public void onCreate() {
 
         super.onCreate();
-        //Метод запускает работу этого объекта. Инициализирует его.
+        //Метод запускает работу этого объекта.Инициализирует его.
         VKSdk.initialize(this);
     }
 }
